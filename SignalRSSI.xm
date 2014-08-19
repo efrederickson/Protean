1d0b7dbaefdbe1dba14768047653a641924a3d5b
Elijah Frederickson <elijah.frederickson@gmail.com>
2014-08-18 21:41:52 -0800

    Added missing files

---
+++ SignalRSSI.xm
@@ -0,0 +1,31 @@
+#import "Protean.h"
+
+@class _UILegibilityImageSet;
+
+@interface UIStatusBarSignalStrengthItemView : UIStatusBarItemView {
+    int _signalStrengthRaw;
+    int _signalStrengthBars;
+    BOOL _enableRSSI;
+    BOOL _showRSSI;
+}
+
+- (NSString *)_stringForRSSI;
+- (_UILegibilityImageSet *)contentsImage;
+- (BOOL)updateForNewData:(id)arg1 actions:(int)arg2;
+- (void)touchesEnded:(id)arg1 withEvent:(id)arg2;
+@end
+
+%hook UIStatusBarSignalStrengthItemView
+- (_UILegibilityImageSet *)contentsImage 
+{
+    CHECK_ENABLED(%orig);
+	
+    id showRSSI = [Protean getOrLoadSettings][@"showSignalRSSI"];
+    if (showRSSI && [showRSSI boolValue])
+    {
+		return [self imageWithText:[self _stringForRSSI]];
+	}
+    
+    return %orig;
+}
+%end
---
+++ proteansettings/PRMakersController.mm
@@ -0,0 +1,101 @@
+#import <Preferences/Preferences.h>
+#import <SettingsKit/SKListControllerProtocol.h>
+#import <SettingsKit/SKTintedListController.h>
+#import <SettingsKit/SKStandardController.h>
+#import <SettingsKit/SKPersonCell.h>
+#import <SettingsKit/SKSharedHelper.h>
+
+@interface PRMakersListController : SKTintedListController<SKListControllerProtocol>
+@end
+@interface PRElijahPersonCell : SKPersonCell
+@end
+@interface PRAndrewPersonCell : SKPersonCell
+@end
+
+@implementation PRElijahPersonCell
+-(NSString*)personDescription { return @"The Developer"; }
+-(NSString*)name { return @"Elijah Frederickson"; }
+-(NSString*)imageName { return @"elijah.png"; }
+@end
+
+@implementation PRAndrewPersonCell
+-(NSString*)personDescription { return @"The Designer"; }
+-(NSString*)name { return @"Andrew Abosh"; }
+-(NSString*)imageName { return @"andrew.png"; }
+@end
+
+@implementation PRMakersListController
+//-(UIColor*) navigationTintColor { return [UIColor colorWithRed:11/255.0f green:234/255.0f blue:241/255.0f alpha:1.0f]; }
+-(BOOL) showHeartImage { return NO; }
+-(NSString*) customTitle { return @"Credits"; }
+
+- (id)customSpecifiers {
+    return @[
+             @{ @"cell": @"PSGroupCell" },
+             @{
+                 @"cell": @"PSLinkCell",
+                 @"cellClass": @"PRElijahPersonCell",
+                 @"height": @100,
+                 @"action": @"openElijahTwitter"
+                 },
+             @{ @"cell": @"PSGroupCell" },
+             @{
+                 @"cell": @"PSLinkCell",
+                 @"cellClass": @"PRAndrewPersonCell",
+                 @"height": @100,
+                 @"action": @"openAndrewTwitter"
+                 },
+             @{ @"cell": @"PSGroupCell" },
+             @{
+                 @"cell": @"PSLinkCell",
+                 @"label": @"Source Code",
+                 @"action": @"openGithub",
+                 @"icon": @"github.png"
+                 },
+             
+             
+             @{ @"cell": @"PSGroupCell",
+                @"label": @"Recommended Themes" },
+             @{
+                 @"cell": @"PSLinkCell",
+                 @"label": @"OpenNotifier iOS 7 Pack",
+                 @"action": @"openRCRepoPack",
+                 @"icon": @"rcrepopack.png"
+                 },
+             
+             
+             @{ @"cell": @"PSGroupCell",
+                @"footerText": @"Acknowledgments: \n\
+\n\
+Julian Weiss - StatusBar refresh animation\n\
+Bensge - UIStatusBar research\n\
+n00neimp0rtant & tateu - OpenNotifier\n\n\
+Situ Herrera, Pele Saeng-a-loon Chaengsavang, Icons8, Allan McAvoy, and Alessio Atzeni - Provided some glyphs and inspiration.\n\n\
+All of you for the tremendous support.\n\
+\n\
+\n\
+Beta testers:\n\
+CPDigitalDarkroom\n\
+hellomisterjedi\n\
+hodhr\n\
+/u/narcolepticinsomniac\n\
+/u/binders_of_women_",
+                },
+             ];
+}
+
+-(void) openGithub
+{
+    [SKSharedHelper openGitHub:@"mlnlover11/Protean"];
+}
+
+-(void) openElijahTwitter
+{
+    [SKSharedHelper openTwitter:@"daementor"];
+}
+
+-(void) openAndrewTwitter
+{
+    [SKSharedHelper openTwitter:@"drewplex"];
+}
+@end
